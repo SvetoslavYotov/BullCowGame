60ea71701007a4cf4b74d7a4838c496db538cf4e
SvetoslavYotov <syotov89@gmail.com>
2019-11-08 16:44:15 +0200

    03. First comparison.

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -4,10 +4,23 @@
 void UBullCowCartridge::BeginPlay() // When the game starts
 {
     Super::BeginPlay();
-	PrintLine("Welcome to Bulls and Cows!\nA fun word game!");
+	PrintLine(TEXT("Welcome to Bulls and Cows!\nA fun word game!"));
+	PrintLine(TEXT("Guess the 6 letter word."));
 }
 
 void UBullCowCartridge::OnInput(const FString& Input) // When the player hits enter
 {
 	ClearScreen();
+	FString HiddenWord = TEXT("planet");
+	if (Input == HiddenWord)
+	{
+		PrintLine(TEXT("Congratulations, you have won the game!"));
+	}
+	else
+	{
+		PrintLine(TEXT("Wrong guess! Sorry, you lose."));
+	}
+	
+
+
 }
