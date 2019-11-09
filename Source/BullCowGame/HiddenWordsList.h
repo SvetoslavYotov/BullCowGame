99256389f3044973d78a9b53e5f1dc84f685575a
SvetoslavYotov <syotov89@gmail.com>
2019-11-09 23:58:51 +0200

    08. Adding list of words.

---
+++ Source/BullCowGame/HiddenWordsList.h
@@ -0,0 +1,34 @@
+#pragma once
+#include "CoreMinimal.h"
+
+const TArray<FString> HiddenWordsList =
+{
+	TEXT("planet"),
+	TEXT("plane"),
+	TEXT("podka"),
+	TEXT("ice"),
+	TEXT("whiskey"),
+	TEXT("whole"),
+	TEXT("whale"),
+	TEXT("fox"),
+	TEXT("dog"),
+	TEXT("cat"),
+	TEXT("bear")
+	TEXT("dear"),
+	TEXT("ocean"),
+	TEXT("nose"),
+	TEXT("mouth"),
+	TEXT("finger"),
+	TEXT("toe"),
+	TEXT("uncle"),
+	TEXT("father"),
+	TEXT("mother"),
+	TEXT("brother"),
+	TEXT("aunt"),
+	TEXT("cousin"),
+	TEXT("able"),
+	TEXT("about"),
+	TEXT("above"),
+	TEXT("below")
+};
+
